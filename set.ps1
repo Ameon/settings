7f91389ecd973fd8b904e29d298e39142707e4a7
Ameon <ameon@ya.ru>
2022-09-28 10:31:24 +0500

    update

--- set.ps1
+++ set.ps1
@@ -6,36 +6,23 @@ function Get-GitCommit { & git commit -m "update"}
 function Get-StartPhpMyadmin { & php -S pma.loc:52175 }
 function Get-StartGoLoc { & php -S go.loc:52176 }
 function Get-StartGoUpdate { & php -S go.update:52177 }
+function Get-Docs { & npm run docs}
 function Get-StartOldGo { & php -S oldgo.loc:52178 }
 function Get-Gpu { & git push }
 function Get-Gal { & git add .}
 function Get-SendMaster{ & ssh master 'cd /var/projects/go/www/go.ams74.ru && git pull origin php8.1'}
 function Get-SendDev{ & ssh master 'cd /var/projects/go/www/dev.go.ams74.ru && git pull origin php8.1'}
-function Get-Auto { 
-  Get-GitAdd;
-  Get-GitCommit;
-  Get-Gpu;
-  Get-SendMaster;
-}
-function Get-AutoDev{
-  Get-GitAdd;
-  Get-GitCommit;
-  Get-Gpu;
-  Get-SendDev;
-}
-function Get-Push{
-  Get-GitAdd;
-  Get-GitCommit;
-  Get-Gpu;
-}
-function Get-Start {
-  & npm run start
-}
-
+function Get-Auto { Get-GitAdd; Get-GitCommit; Get-Gpu; Get-SendMaster; }
+function Get-AutoDev{ Get-GitAdd; Get-GitCommit; Get-Gpu; Get-SendDev; }
+function Get-Proj { set-location "C:/proj" }
+function Get-Push{ Get-GitAdd; Get-GitCommit; Get-Gpu; }
+function Get-Start { & npm run start }
+function Get-StartWatch { & npm run start:watch }
 
 Set-Alias 'a' Get-Auto
 Set-Alias 'adev' Get-AutoDev
 Set-Alias 'c' cls
+Set-Alias 'docs' Get-Docs
 Set-Alias 'eba' Get-EditAliases
 Set-Alias 'g' Get-GitInit
 Set-Alias 'ga' Get-GitAdd
@@ -46,7 +33,9 @@ Set-Alias 'gpu' Get-Gpu
 Set-Alias 'gs' Get-GitStatus
 Set-Alias 'oldgo' Get-StartOldGo
 Set-Alias 'pma' Get-StartPhpMyadmin
+Set-Alias 'proj' Get-Proj
 Set-Alias 'push' Get-Push
 Set-Alias 's' Get-Start
+Set-Alias 'sw' Get-StartWatch
 
 
