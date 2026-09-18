3a97c70b42010bcf4352263c34294a42b86b0be2
Ameon <ameon@ya.ru>
2026-09-18 06:43:08 +0500

    update

--- set.ps1
+++ set.ps1
@@ -1,6 +1,7 @@
 $PSDefaultParameterValues['Out-File:Encoding'] = 'UTF8'
 $OutputEncoding = [System.Text.Encoding]::UTF8
 
+Remove-Item Alias:gl -Force -ErrorAction SilentlyContinue
 Import-Module ControlAliases
 Import-Module ControlFiles
 Import-Module ControlFolders
